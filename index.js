6cbb5c4fbc06d887e2cf0fadf92d79b5929655b9
BMK53 <152314912+BMK53@users.noreply.github.com>
2024-04-21 17:18:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/ok', (req, res) => {
     res.status(200).send('ok')
   })
 
-app.use("/api/Tasks", Tasks);
+app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 3500;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
